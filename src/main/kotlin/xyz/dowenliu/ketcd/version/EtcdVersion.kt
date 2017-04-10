0f029ba554cd80c296d3f28d7538fd1f29d76bb1
dowenliu-xyz <hawkdowen@hotmail.com>
2017-04-10 16:01:37 +0800

    add: enum class EtcdVersion
    Known etcd version, since v3.0.0.

---
+++ src/main/kotlin/xyz/dowenliu/ketcd/version/EtcdVersion.kt
@@ -0,0 +1,39 @@
+package xyz.dowenliu.ketcd.version
+
+/**
+ * Known etcd version, since v3.0.0.
+ *
+ * create at 2017/4/10
+ * @author liufl
+ * @since 0.1.0
+ */
+enum class EtcdVersion {
+    V3_0_0,
+    V3_0_1,
+    V3_0_2,
+    V3_0_3,
+    V3_0_4,
+    V3_0_5,
+    V3_0_6,
+    V3_0_7,
+    V3_0_8,
+    V3_0_9,
+    V3_0_10,
+    V3_0_11,
+    V3_0_12,
+    V3_0_13,
+    V3_0_14,
+    V3_0_15,
+    V3_0_16,
+    V3_0_17,
+    V3_1_0_alpha0,
+    V3_1_0_alpha1,
+    V3_1_0_rc0,
+    V3_1_0_rc1,
+    V3_1_0,
+    V3_1_1,
+    V3_1_2,
+    V3_1_3,
+    V3_1_4,
+    V3_1_5,
+}

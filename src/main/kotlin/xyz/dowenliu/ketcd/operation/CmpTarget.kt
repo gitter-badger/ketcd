deee1441d8e4aa136e1954c78710daba62c078f3
dowenliu-xyz <hawkdowen@hotmail.com>
2017-04-11 12:59:59 +0800

    add: class CmpTarget
    Compare target

---
+++ src/main/kotlin/xyz/dowenliu/ketcd/operation/CmpTarget.kt
@@ -0,0 +1,64 @@
+package xyz.dowenliu.ketcd.operation
+
+import com.google.protobuf.ByteString
+import xyz.dowenliu.ketcd.api.Compare
+
+/**
+ * Cmp  target used in [Txn]
+ *
+ * create at 2017/4/11
+ * @author liufl
+ * @since 0.1.0
+ *
+ * @param T The compare target value type of this compare.
+ *
+ * @property target The compare target used for this compare.
+ * @property targetValue The compare target value of this compare.
+ */
+abstract class CmpTarget<out T>(val target: Compare.CompareTarget, val targetValue: T) {
+    companion object {
+        /**
+         * Cmp on a given _version_
+         *
+         * @param version version to compare.
+         * @return the version compare target.
+         */
+        fun version(version: Long): VersionCmpTarget = VersionCmpTarget(version)
+
+        /**
+         * Cmp on the create _revision_
+         *
+         * @param revision the create revision
+         * @return the create revision compare target.
+         */
+        fun createVersion(revision: Long): CreateRevisionCmpTarget = CreateRevisionCmpTarget(revision)
+
+        /**
+         * Cmp on the modification _revision_
+         *
+         * @param revision the modification revision
+         * @return the modification revision compare target.
+         */
+        fun modRevision(revision: Long): ModRevisionCmpTarget = ModRevisionCmpTarget(revision)
+
+        /**
+         * Cmp on the _value_
+         *
+         * @param value the value to compare.
+         * @return the value compare target.
+         */
+        fun value(value: ByteString): ValueCmpTarget = ValueCmpTarget(value)
+    }
+
+    class VersionCmpTarget internal constructor(targetValue: Long) :
+            CmpTarget<Long>(Compare.CompareTarget.VERSION, targetValue)
+
+    class CreateRevisionCmpTarget internal constructor(targetValue: Long) :
+            CmpTarget<Long>(Compare.CompareTarget.CREATE, targetValue)
+
+    class ModRevisionCmpTarget internal constructor(targetValue: Long) :
+            CmpTarget<Long>(Compare.CompareTarget.MOD, targetValue)
+
+    class ValueCmpTarget internal constructor(targetValue: ByteString) :
+            CmpTarget<ByteString>(Compare.CompareTarget.VALUE, targetValue)
+}
